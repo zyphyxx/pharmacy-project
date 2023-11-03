e6a660620e30eade2d8ca5498dd1237badfe91a6
Edgar Jesus <mendesbr92@gmail.com>
2023-11-03 17:55:52 -0300

    agora com o responseEntity

--- almoxarifado/src/main/java/com/zpx/almoxarifado/controller/ProductController.java
+++ almoxarifado/src/main/java/com/zpx/almoxarifado/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.zpx.almoxarifado.controller;
 import com.zpx.almoxarifado.dto.ProductDao;
 import com.zpx.almoxarifado.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/")
@@ -22,14 +24,17 @@ public class ProductController {
         this.service = service;
     }
 
+
     @GetMapping
-    public List<ProductDao> listarProdutos() {
-        return service.listarProduto();
+    public ResponseEntity<List<ProductDao>> listarProdutos() {
+        return ResponseEntity.ok(service.listarProduto());
     }
 
+
     @GetMapping("/{id}")
-    public Optional<ProductDao> listarPorId(@PathVariable Long id) {
-        return service.encontrarPeloId(id);
+    public ResponseEntity<Optional<ProductDao>> listarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(service.encontrarPeloId(id));
     }
+
 
 }
